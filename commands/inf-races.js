e817b0f8bb2fd015d188c3d660f33183daa52455
Kesley Nascimento <kesley.beta@outlook.com>
2019-01-07 17:27:50 -0200

    190107-1727

--- commands/inf-races.js
+++ commands/inf-races.js
@@ -1,38 +1,42 @@
 const Discord = require("discord.js");
 const fs = require("fs");
+const mongoose = require("mongoose");
 const modRaces = require("../models/mod-races.js");
 let jsonRaces = JSON.parse(fs.readFileSync("./jsonfiles/races.json", 'utf8'));
 
 module.exports.run = async (bot, message, args, cmd) => {
     //await message.delete();
-    console.log(`[CMD] ${cmd.slice(1)} [MSG] ${args || "empty"} | requested by: [${message.author.tag}]`);
+    let specificRace = args[0].toString();
+    console.log(`[CMD] ${cmd.slice(1)} [MSG] ${args.toString() || "empty"} | requested by: [${message.author.tag}]`);
 
-    if(args.toString() === `Dwarf`){
+    if (!args.toString()) {
+        let rEmbed = new Discord.RichEmbed()
+            .setTitle("ALL RACES FROM FIFTH EDITION OF THE PLAYER'S HANDBOOK")
+            .setColor("#10E0FF")
+            .setDescription(`\`\`\`css\n${jsonRaces.allraces.toString().split(",").join(", ")}\`\`\``);
+
+        return message.channel.send(rEmbed);
+    } 
+    if(args.toString()) {
         console.log(args);
         let dembed = new Discord.RichEmbed()
             .setTitle("RACE'S MANUAL")
-            .setColor("#DD69D6");
+            .setColor("#DD69D6");           
 
-        modRaces.findOne({
-            _id: "5c315c02dd843e0698842af5"
-        }, (err, races) => {
+        modRaces.find({}, (err, races) => {
             if (err) console.log("[ERR] " + err);
+            console.log(races)
             if (!races) {
                 dembed.setDescription("Couldn't find any information.")
-                return message.channel.send(dembed);
+                return message.channel.send(dembed)
             } else {
-                dembed.setDescription(races.Dwarf.description.toString(), true);
+                //console.log(races.raceslist)
+
+                //dembed.setTitle(races.name.toString())//.setDescription(races.description.toString());
                 return message.channel.send(dembed);
             }
         })
     }
-
-    let rEmbed = new Discord.RichEmbed()
-    .setTitle("RACES FROM FIFTH EDITION OF THE PLAYER'S HANDBOOK")
-    .setColor("#10E0FF")
-    .addField(`🧙`, jsonRaces.allraces.toString().split(",").join(", "), true);
-    
-    return message.channel.send(rEmbed);
 }
 
 module.exports.config = {
--- models/mod-races.js
+++ models/mod-races.js
@@ -1,18 +1,16 @@
 const mongoose = require("mongoose");
 
 const raceSchema = mongoose.Schema({
-    allraces: Array,
-    Dwarf: {
-        description: String,
-        abilityscore: { ability: 0 },        
-        age: {adult: Number, max: Number},
-        alignment: String,
-        size: String,        
-        speed: 0,
-        languages: Array,        
-        features: Array,
-        subraces: Array,
-    }
+    name: String,
+    description: String,
+    abilityscore: {str: Number, dex: Number, con: Number, int: Number, wis: Number, cha: Number },
+    age: { adult: Number, max: Number },
+    alignment: String,
+    size: String,
+    speed: Number,
+    languages: Array,
+    features: Array,
+    subraces: Array
 })
 
 module.exports = mongoose.model("races", raceSchema);
