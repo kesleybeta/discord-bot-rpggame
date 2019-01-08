3b685d2f04b15f411aef20d483e0dc318341997e
Kesley Nascimento <kesley.beta@outlook.com>
2019-01-08 15:04:40 -0200

    190108-1504

--- commands/inf-races.js
+++ commands/inf-races.js
@@ -1,39 +1,51 @@
-const Discord = require("discord.js");
-const fs = require("fs");
-const mongoose = require("mongoose");
-const modRaces = require("../models/mod-races.js");
-let jsonRaces = JSON.parse(fs.readFileSync("./jsonfiles/races.json", 'utf8'));
+const Discord = require("discord.js")
+const fs = require("fs")
+const modRaces = require("../models/mod-races.js")
+let jsonRaces = JSON.parse(fs.readFileSync("./jsonfiles/races.json", 'utf8'))
+
+function capFirst(string) {
+    return string[0].toUpperCase() + string.slice(1)
+}
 
 module.exports.run = async (bot, message, args, cmd) => {
-    //await message.delete();
-    let specificRace = args[0].toString();
-    console.log(`[CMD] ${cmd.slice(1)} [MSG] ${args.toString() || "empty"} | requested by: [${message.author.tag}]`);
+    await message.delete();
+    console.log(`[${cmd.slice(1)}] requested by: [${message.author.tag}]`)
 
     if (!args.toString()) {
         let rEmbed = new Discord.RichEmbed()
             .setTitle("ALL RACES FROM FIFTH EDITION OF THE PLAYER'S HANDBOOK")
             .setColor("#10E0FF")
-            .setDescription(`\`\`\`css\n${jsonRaces.allraces.toString().split(",").join(", ")}\`\`\``);
-
-        return message.channel.send(rEmbed);
-    } 
-    if(args.toString()) {
-        console.log(args);
+            .setDescription(`\`\`\`css\n${jsonRaces.allraces.toString().split(",").join(", ")}\`\`\``)
+        return message.channel.send(rEmbed)
+    }
+    if (args.toString()) {
+        let specificRace = capFirst(args[0].toString())
         let dembed = new Discord.RichEmbed()
             .setTitle("RACE'S MANUAL")
-            .setColor("#DD69D6");           
+            .setColor("#5D69D6")
 
-        modRaces.find({}, (err, races) => {
-            if (err) console.log("[ERR] " + err);
-            console.log(races)
-            if (!races) {
+        modRaces.findOne({
+            name: specificRace
+        }, (err, result) => {
+            if (err) console.log("[ERR] " + err)
+            if (result === null) return message.reply(`Please, give a valid RACE! \`${cmd} <race>\``)
+            if (!result) {
                 dembed.setDescription("Couldn't find any information.")
                 return message.channel.send(dembed)
             } else {
-                //console.log(races.raceslist)
-
-                //dembed.setTitle(races.name.toString())//.setDescription(races.description.toString());
-                return message.channel.send(dembed);
+                dembed
+                    .setThumbnail(`${result.iconurl}`)
+                    .setDescription(`${result.description}`)
+                    .addField("Name", `${result.name} `, true)
+                    .addField("Age", `Adult: **${result.age.adult}** | Max: **${result.age.max}** `, true)
+                    .addField("Ability score increase", `*STR*: **${result.abilityscore.str}** | *DEX*: **${result.abilityscore.dex}** | *CON*: **${result.abilityscore.con}** | *INT*: **${result.abilityscore.int}** | *WIS*: **${result.abilityscore.wis}** | *CHA*: **${result.abilityscore.cha}**`)
+                    .addField("Alignment", `${result.alignment} `, true)
+                    .addField("Features", `${result.features} `, true)
+                    .addField("Languages", `${result.languages} `, true)
+                    .addField("Size", `${result.size} `, true)
+                    .addField("Speed", `${result.speed} `, true)
+                    .addField("Subraces", `${result.subraces} `, true)
+                return message.channel.send(dembed)
             }
         })
     }
--- models/mod-races.js
+++ models/mod-races.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 
 const raceSchema = mongoose.Schema({
-    name: String,
-    description: String,
     abilityscore: {str: Number, dex: Number, con: Number, int: Number, wis: Number, cha: Number },
     age: { adult: Number, max: Number },
     alignment: String,
+    description: String,
+    features: Array,
+    iconurl: String,
+    languages: Array,
+    name: String,
     size: String,
     speed: Number,
-    languages: Array,
-    features: Array,
     subraces: Array
 })
 
