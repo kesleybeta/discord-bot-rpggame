f4141b444950db683887dbb92cea4229a9a39a24
Kesley Nascimento <kesley.beta@outlook.com>
2019-01-08 17:18:05 -0200

    190108-1718

--- commands/inf-races.js
+++ commands/inf-races.js
@@ -3,10 +3,6 @@ const fs = require("fs")
 const modRaces = require("../models/mod-races.js")
 let jsonRaces = JSON.parse(fs.readFileSync("./jsonfiles/races.json", 'utf8'))
 
-function capFirst(string) {
-    return string[0].toUpperCase() + string.slice(1)
-}
-
 module.exports.run = async (bot, message, args, cmd) => {
     await message.delete();
     console.log(`[${cmd.slice(1)}] requested by: [${message.author.tag}]`)
@@ -16,18 +12,28 @@ module.exports.run = async (bot, message, args, cmd) => {
             .setTitle("ALL RACES FROM FIFTH EDITION OF THE PLAYER'S HANDBOOK")
             .setColor("#10E0FF")
             .setDescription(`\`\`\`css\n${jsonRaces.allraces.toString().split(",").join(", ")}\`\`\``)
-        return message.channel.send(rEmbed)
+
+        //return message.channel.send(rEmbed)
+        modRaces.find({edition: "5th" }).then(function (res) {
+            let tudo = []
+            tudo.push(res[0].name)
+            for(i=1;i < res.length-1; i++)
+                tudo.unshift(res[i].name)
+            });
+            console.log(tudo)
     }
+
     if (args.toString()) {
-        let specificRace = capFirst(args[0].toString())
+        let specificRace = args.toString().toLowerCase().split(",").join(" ")
+        console.log(specificRace)
         let dembed = new Discord.RichEmbed()
             .setTitle("RACE'S MANUAL")
             .setColor("#5D69D6")
 
         modRaces.findOne({
-            name: specificRace
+            namel: specificRace
         }, (err, result) => {
-            if (err) console.log("[ERR] " + err)
+            if (err) console.log("[2] " + err)
             if (result === null) return message.reply(`Please, give a valid RACE! \`${cmd} <race>\``)
             if (!result) {
                 dembed.setDescription("Couldn't find any information.")
--- models/mod-profile.js
+++ models/mod-profile.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const profileSchema = mongoose.Schema({
-    userID: String
+    userID: String,
+    userTag: String,
+    serverID: String,
+    characters: { type: Object }
 })
 
 module.exports = mongoose.model("char-profile", profileSchema);
