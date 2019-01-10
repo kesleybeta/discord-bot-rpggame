bc9223a705466acfac87999ebcecbdeaf2ab66f5
Kesley Nascimento <kesley.beta@outlook.com>
2019-01-10 21:46:44 -0200

    190110-2146

--- commands/gam-hello.js
+++ commands/gam-hello.js
@@ -1,20 +1,18 @@
 const Discord = require("discord.js")
-const botconfig = require("../botconfig.json")
 
 module.exports.run = async (bot, message, cmd, args) => {
     await message.delete()
     console.log(`[${cmd.slice(1)}] requested by: [${message.author.tag}]`)
 
     let helemb = new Discord.RichEmbed()
-        //.setAuthor("Game Master", "https://cdn.iconscout.com/icon/premium/png-256-thumb/wizard-23-483776.png")
+        .setAuthor("Game Master", "https://cdn.iconscout.com/icon/premium/png-256-thumb/wizard-23-483776.png")
         .setColor("#008080")
-        .setThumbnail("https://cdn.iconscout.com/icon/premium/png-256-thumb/wizard-23-483776.png")
         .setTitle(`Hello ${message.author.username}`)
-        .addField(`I am the *Game Master*. I'm here to guide you on your adventure. \n Well, If you are ready type **${cmd}start**`)
+        .setDescription(`I am the *GM*.\nI'm here to guide you on your adventure.\nWell, If you are ready then type:\n\n\`\`\`diff\n+[create]\n---To create your new character.\n\`\`\`\n\`\`\`diff\n+[profile]\n---To see your new character profile.\n\`\`\``)
 
     message.channel.send(helemb)
 }
 module.exports.config = {
     name: "hello",
-    aliases: []
+    aliases: ["qck"]
 }
--- commands/gam-profile.js
+++ commands/gam-profile.js
@@ -4,42 +4,41 @@ const profileMod = require("../models/mod-profile.js")
 module.exports.run = async (bot, message, cmd, args) => {
     await message.delete()
     console.log(`[${cmd.slice(1)}] requested by: [${message.author.tag}]`)
-    console.log(message.author.tag+" - "+message.author.id)
 
     let embed = new Discord.RichEmbed()
-        .setColor("#103103")
-        //.setThumbnail(message.author.displayAvatarURL)
-        .setTitle(`Profile Character`)
-        .setDescription("This place will be filled with your characters information.\nYOUR CHARACTER SHEET( POBRE )")
+        .setColor("#FFFFFF")
+        .setTitle(`YOUR CHARACTER SHEET`)
+        .setDescription("This place will be filled with your characters information!")
 
     profileMod.findOne({
         userID: message.author.id,
         serverID: message.guild.id
     }, (err, result) => {
         if (err) return console.log(chalk.bgRedBright(message.author.id+ "GP01 " + err))
-        //if (result === null) return message.reply(message.author.id+ "GP02 - Contact the dev")
+        if (result === null) return message.reply(message.author.id+ "GP02 - Contact the dev")
         if (!result) {
             const newProfile = new profileMod({
                 userID: message.author.id,
                 userTag: message.author.tag,
                 serverID: message.guild.id,
                 characters: {
-                    name: "unknown",
-                    race: "racao",
-                    class: "nenhuma",
-                    background: "wallpaper"
+                    name: "",
+                    race: "",
+                    class: "",
+                    background: ""
                 }
             })
             newProfile.save().catch(err => console.log(chalk.redBright(message.author.id+ "GP03 newProfile.save() > " + err)))
 
             //embed.addField("Log", `You're new here so, a new profile was created.`, true)
-            message.reply(`\nYou're new here so, a new profile was created.\nType \`{}start\` to build your character.`)
+            message.reply(`\nYou're new here so, a new profile was created.\nType \`{}create\` to build your character.`)
         } else {
             embed
+            //.setThumbnail(message.author.displayAvatarURL)
             .addField("Name", `${result.characters.name} `, true)
             .addField("Race", `${result.characters.race} `, true)
             .addField("Class", `${result.characters.class} `, true)
-            .addField("Background", `${result.characters.background} `, true)
+            .addField("Background", `${result.characters.background} `)
             message.channel.send(embed)
         }
     })
@@ -47,5 +46,5 @@ module.exports.run = async (bot, message, cmd, args) => {
 
 module.exports.config = {
     name: "profile",
-    aliases: ["p", "pfl"]
+    aliases: ["sheet"]
 }
